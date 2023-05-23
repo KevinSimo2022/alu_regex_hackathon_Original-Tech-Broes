77cd0ad3bf09caafb83421514ec480b2540328be
root <you@example.com>
2023-05-22 23:36:27 -0700

    Episode_Title.py

--- Episode_Title.py
+++ Episode_Title.py
@@ -9,10 +9,10 @@ and "Episode Title" is any string of characters'''
 
 # Episode titles of movies are listed including their season number and episode number
 episode_titles = ['Money Heist S13E03', 'Money Heist S11E13', 'Money Heist S06E10', 'Money Heist S10E05']
-regex_episode = r"(\w+)\sS(\d{2})E(\d{2}):\s(.*)"
+regex_episode = r"(\w+)\sS(\d{2})E(\d{2})"
 
 for episode_title in episode_titles:
     episode_info = re.findall(regex_episode, episode_title)
     if episode_info:
-        show_name, season_number, episode_number, _ = episode_info[0]
+        show_name, season_number, episode_number = episode_info[0]
         print(f"Show: {show_name}, Season: {season_number}, Episode: {episode_number}")
